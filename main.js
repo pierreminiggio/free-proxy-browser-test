e6def1ce1a0946d380279af82f22981be2e19833
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-01 22:05:16 +0200

    try proxies till one sticks

--- main.js
+++ main.js
@@ -11,9 +11,38 @@ async function fetchProxies() {
     })
 }
 
+/**
+ * @param {?string} proxy 
+ * @param {boolean} show 
+ * 
+ * @returns {Promise<Browser>}
+ */
+async function createBrowser(proxy, show) {
+    return new Promise(async (resolve, rejects) => {
+        let launchParameters = {headless: ! show}
+        if (proxy) {
+            launchParameters.args = ['--proxy-server=socks5://' + proxy]
+        }
+        puppeteer.launch(launchParameters).then(browser => {
+            resolve(browser)
+        }).catch(error => {
+            rejects(error)
+        })
+    })  
+}
+
 async function letsGo() {
     const proxies = await fetchProxies()
-    console.log(proxies)
+    const browser = await createBrowser(proxies[Math.floor(Math.random() * proxies.length)], true)
+    const ipCheckerPage = await browser.newPage()
+    try {
+        await ipCheckerPage.goto('https://api.myip.com')
+    } catch(e) {
+        console.log(e)
+        browser.close()
+        letsGo()
+    }
+    
 }
 
 letsGo()
